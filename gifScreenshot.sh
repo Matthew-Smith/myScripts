64ab8f226600398be6b322f50fde0eadbdded97e
matthewsmith <matthewsmith@espial.com>
2015-11-23 10:16:46 -0500

    Made the gif script copy the location of the output with the PC's IP attached into the clipboard

--- gifScreenshot.sh
+++ gifScreenshot.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-TMP_AVI=$(mktemp /tmp/outXXXXXXXXXX.avi)
+outDirectory='/var/www/html'
+subDir='/Screenshots/'
+outName=$(date +%Y.%m.%d-%H.%M.%S.gif)
+tempIP=$(ifconfig eth0 | grep inet | awk '{print $2}' | cut -d':' -f2)
+IP=$(echo http://"${tempIP}" | tr -d '[[:space:]]')
+
+tempAVI=$(mktemp /tmp/outXXXXXXXXXX.avi)
 ffcast -s % ffmpeg -y -f x11grab -show_region 1 -framerate 15 \
     -video_size %s -i %D+%c -codec:v huffyuv                  \
-    -vf crop="iw-mod(iw\\,2):ih-mod(ih\\,2)" $TMP_AVI         \
-&& convert -set delay 10 -layers Optimize $TMP_AVI out.gif
+    -vf crop="iw-mod(iw\\,2):ih-mod(ih\\,2)" $tempAVI         \
+&& convert -set delay 10 -layers Optimize $tempAVI $outDirectory$subDir$outName
+
+echo $IP$subDir$outName | xclip -selection clipboard
