4caf039d4612507187cda7cb94b9e483a9eee22e
Matthew Smith <matthewsmith@espial.com>
2016-11-02 14:06:46 -0400

    saving the changes I've made for the work laptop

--- screenshot.sh
+++ screenshot.sh
@@ -1,9 +1,12 @@
-root='/var/www/html'
-tempIP=$(ifconfig eth0 | grep inet | awk '{print $2}' | cut -d':' -f2)
+root='/home/matthewsmith/http'
+# tempIP=$(ifconfig eth0 | grep inet | awk '{print $2}' | cut -d':' -f2)
+tempIP=$(ip addr show enp0s31f6 | awk '/inet / {gsub(/\/.*/,"",$2); print $2}')
 IP=$(echo http://"${tempIP}" | tr -d '[[:space:]]')
 subDir='/Screenshots/'
 fileName=$(date +%Y.%m.%d-%H.%M.%S.png)
 
+echo $root$subDir$fileName
 scrot -s $root$subDir$fileName
 
+echo $IP$subDir$fileName
 echo $IP$subDir$fileName | xclip -selection clipboard
